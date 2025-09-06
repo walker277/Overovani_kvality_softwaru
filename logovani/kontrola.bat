50ced53d742722ee8b1576ace8d80fda8222eade
walker277 <146030713+walker277@users.noreply.github.com>
2025-09-06 12:17:11 +0200

    Add files via upload

---
+++ logovani/kontrola.bat
@@ -0,0 +1,5 @@
+del oks-01-log*.txt
+
+java -cp log4j-api-2.11.1.jar;log4j-core-2.11.1.jar;. oks01.Hlavni neexistujici.txt fav
+java -cp log4j-api-2.11.1.jar;log4j-core-2.11.1.jar;. oks01.Hlavni priklady-oks-01-2.txt fav
+java -cp log4j-api-2.11.1.jar;log4j-core-2.11.1.jar;. oks01.Hlavni priklady-oks-01-1.txt fav
